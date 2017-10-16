bd8d723d6e8292b885800d2fb3b70ea49d6f2e6c
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-16 13:01:25 -0500

    A new File to explain the multiply logic was added.

---
+++ Basic_Java/src/com/javabasicsforbegineers/Multiply.java
@@ -0,0 +1,12 @@
+package com.javabasicsforbegineers;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 50;
+		int product = a * b;
+		System.out.println("The product of given two numbers is : " + product);
+	}
+
+}
