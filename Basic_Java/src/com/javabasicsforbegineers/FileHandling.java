c8d06426f904912ffe1c791e6ceef95f9b9deafb
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-18 11:47:00 -0500

    Sample code with examples on File Handling in Java Programming Language was added to the FileHandling.java File.

--- Basic_Java/src/com/javabasicsforbegineers/FileHandling.java
+++ Basic_Java/src/com/javabasicsforbegineers/FileHandling.java
@@ -1,9 +1,60 @@
 package com.javabasicsforbegineers;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileHandling {
-
-	public static void main(String[] args) {
-
+	
+	public static void CreateFolder()
+	{
+		File fo = new File("C:/Users/PrabhuPushpaKumar/Desktop/SeleniumFolder");
+		fo.mkdir();
+	}
+	
+	public static void CheckFolderExists()
+	{
+		File fo = new File("C:/Users/PrabhuPushpaKumar/Desktop/SeleniumFolder");
+		fo.mkdir();
+		boolean b = fo.exists();
+		if (b==true)
+		{
+			System.out.println("The Folder Exists on your Machine");
+		}
+		else
+		{
+			System.out.println("The Folder Not Exists on your Machine");
+		}
+	}
+	
+	public static void DeleteFolder()
+	{
+		File fo1 = new File("C:/Users/PrabhuPushpaKumar/Desktop/SeleniumFolder_1");
+		fo1.mkdir();
+		fo1.delete();
+	}
+	
+	public static void CreateNewFile() throws IOException
+	{
+		File fo1 = new File("C:/Users/PrabhuPushpaKumar/Desktop/SampleFolder_1");
+		fo1.mkdir();
+		File fo2 = new File("C:/Users/PrabhuPushpaKumar/Desktop/SampleFolder_1/sample.txt");
+		fo2.createNewFile();
+	}
+	
+	public static void DeleteFile() throws IOException
+	{
+		File fo1 = new File("C:/Users/PrabhuPushpaKumar/Desktop/SampleFolder_1");
+		fo1.mkdir();
+		File fo2 = new File("C:/Users/PrabhuPushpaKumar/Desktop/SampleFolder_1/sample1.txt");
+		fo2.createNewFile();
+		fo2.delete();
+	}
+	public static void main(String[] args) throws IOException {
+		//CreateFolder;
+		//CheckFolderExists;
+		//DeleteFolder();
+		//CreateNewFile();
+		DeleteFile();
 	}
 
 }
