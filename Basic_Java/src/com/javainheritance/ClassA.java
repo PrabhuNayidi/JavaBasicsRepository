bf55848e0cc1381bbc73769bbc8cd2d8383fab89
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 18:44:12 -0500

    Some code was added to the ClassA file.

--- Basic_Java/src/com/javainheritance/ClassA.java
+++ Basic_Java/src/com/javainheritance/ClassA.java
@@ -1,26 +1,34 @@
 package com.javainheritance;
 
 public class ClassA {
-
+	/* Types of Inheritance in Java Programming Language:
+	  Single Inheritance - 
+	  			public Class ClassB extends ClassA
+	  			{
+	  			}
+	  Multi Level Inheritance - 
+	  			public Class ClassB extends ClassA {
+	  			public Class ClassC extends ClassB {
+	  			}
+	  			}
+	  Note : Java Language doesn't support Multiple Inheritance due to Diamond Problem
+	  Multiple Inheritance - 
+	  public Class ClassB extends ClassA {
+	  public Class ClassB extends ClassC {
+	  }
+	  }
+	*/
+	int a = 25;
+	int b =50;
+	public void add()
+	{
+		System.out.println("The Sum of given two numbers is : " + (a+b));
+	}
 	public static void main(String[] args) {
-		/* Types of Inheritance in Java Programming Language:
-		  Single Inheritance - 
-		  			public Class ClassB extends ClassA
-		  			{
-		  			}
-		  Multi Level Inheritance - 
-		  			public Class ClassB extends ClassA {
-		  			public Class ClassC extends ClassB {
-		  			}
-		  			}
-		  Note : Java Language doesn't support Multiple Inheritance due to Diamond Problem
-		  Multiple Inheritance - 
-		  public Class ClassB extends ClassA {
-		  public Class ClassB extends ClassC {
-		  }
-		  }
-		*/
-
+		ClassA o1 = new ClassA();
+		System.out.println("The First Number is : " + o1.a);
+		System.out.println("The Second Number is : " + o1.b);
+		o1.add();
 	}
 
 }
