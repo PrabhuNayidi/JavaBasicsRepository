134e6566b3a85e504053ff862267f10b6bb734c2
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 18:49:01 -0500

    Some code was added to ClassB.java file to explain the Inheritance Concept in Java Programming Language.

--- Basic_Java/src/com/javainheritance/ClassB.java
+++ Basic_Java/src/com/javainheritance/ClassB.java
@@ -1,9 +1,11 @@
 package com.javainheritance;
 
-public class ClassB {
+public class ClassB extends ClassA{
 
 	public static void main(String[] args) {
-
+		ClassB o2 = new ClassB();
+		System.out.println("The value of a from ClassA is : " + o2.a);
+		System.out.println("The value of b from ClassA is : " + o2.b);
+		o2.add();
 	}
-
 }
