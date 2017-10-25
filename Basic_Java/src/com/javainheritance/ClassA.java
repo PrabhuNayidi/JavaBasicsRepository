2184075eb8152a9517ea4ff0f413716b008eb5ee
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 18:39:59 -0500

    New Package was created with name com.javainheritance under src folder.
    Comments for Inheritance Types were added to the file ClassA.java.

---
+++ Basic_Java/src/com/javainheritance/ClassA.java
@@ -0,0 +1,26 @@
+package com.javainheritance;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		/* Types of Inheritance in Java Programming Language:
+		  Single Inheritance - 
+		  			public Class ClassB extends ClassA
+		  			{
+		  			}
+		  Multi Level Inheritance - 
+		  			public Class ClassB extends ClassA {
+		  			public Class ClassC extends ClassB {
+		  			}
+		  			}
+		  Note : Java Language doesn't support Multiple Inheritance due to Diamond Problem
+		  Multiple Inheritance - 
+		  public Class ClassB extends ClassA {
+		  public Class ClassB extends ClassC {
+		  }
+		  }
+		*/
+
+	}
+
+}
