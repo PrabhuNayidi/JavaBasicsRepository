09a6310b0a44f6e2ba51d11ddb7ed8660fff244f
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 11:56:50 -0500

    Sample code was added to the Car.java file.

--- Basic_Java/src/com/javaabstraction/Car.java
+++ Basic_Java/src/com/javaabstraction/Car.java
@@ -1,7 +1,16 @@
 package com.javaabstraction;
 
-public class Car {
-
+public abstract class Car {
+	public void doors()
+	{
+		System.out.println("The Sedan Car have 4 Doors.");
+	}
+	public void types()
+	{
+		System.out.println("There are 6 types of Sedans.");
+	}
+	public abstract void engine();
+	public abstract void name();
 	public static void main(String[] args) {
 
 	}
