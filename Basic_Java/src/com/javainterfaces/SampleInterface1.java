3d81368306a94128dde6210f95f8a68da246d0ad
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:25:20 -0500

    Code for accessing the implemented Abstract Methods from the Interface(SampleInterface1.java).

--- Basic_Java/src/com/javainterfaces/SampleInterface1.java
+++ Basic_Java/src/com/javainterfaces/SampleInterface1.java
@@ -5,4 +5,12 @@ public interface SampleInterface1 {
 	public void gender();
 	public void age();
 	public void address();
+
+public static void main(String[] args) {
+	SampleClass1 o1 = new SampleClass1();
+	o1.myName();
+	o1.gender();
+	o1.age();
+	o1.address();
+}
 }
