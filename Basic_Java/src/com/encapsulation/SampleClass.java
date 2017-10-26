60d4f9ffa5c8cadee213aa382ba95295693acdeb
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:43:41 -0500

    Sample code was added to SampleClass.java file.

--- Basic_Java/src/com/encapsulation/SampleClass.java
+++ Basic_Java/src/com/encapsulation/SampleClass.java
@@ -9,8 +9,20 @@ public class SampleClass {
 	 		I. It Provides control over the Data.
 	 		II. By providing setter and getter Methods we can make a Class Read only or Write only.
 	*/
+	
+	//Create a private Variable of Data Type String.
+	private String courseName = "CoreJava";
+	
+	//Write getter Method.
+	public String getName()
+	{
+		return courseName;
+	}
+	
+	//Write setter Method
 	public static void main(String[] args) {
-
+		SampleClass obj = new SampleClass();
+		System.out.println(obj.getName()); // getter Method Without setter Method.
 	}
 
 }
