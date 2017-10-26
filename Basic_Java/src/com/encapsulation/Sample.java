22ceb9ea2b2a76ab84e511d91cdb940361eefab1
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:49:41 -0500

    Accessing a private field in private method using getName() in Child Class Sample.java

--- Basic_Java/src/com/encapsulation/Sample.java
+++ Basic_Java/src/com/encapsulation/Sample.java
@@ -4,6 +4,7 @@ public class Sample extends SampleClass{
 
 	public static void main(String[] args) {
 		Sample o = new Sample();
+		System.out.println(o.getName());//Using getter Method we are accessing the private field(Variable courseName).
 	}
 
 }
