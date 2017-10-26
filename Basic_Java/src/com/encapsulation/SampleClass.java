e386322e6ed9e01c8ef8b6803b8d92aa8b7729b2
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:37:17 -0500

    Few important points on Encapsulation concept were added to the file SampleClass.java in com.encapsulation package.

--- Basic_Java/src/com/encapsulation/SampleClass.java
+++ Basic_Java/src/com/encapsulation/SampleClass.java
@@ -2,6 +2,13 @@ package com.encapsulation;
 
 public class SampleClass {
 
+	/* Encapsulation - 
+	 	I. It is a Process of Binding or wrapping the Code and Data into a Single Unit.
+	 	II. It is a technique for making the fields in a Class private and providing the access through public Methods.
+	 	Advantages:
+	 		I. It Provides control over the Data.
+	 		II. By providing setter and getter Methods we can make a Class Read only or Write only.
+	*/
 	public static void main(String[] args) {
 
 	}
