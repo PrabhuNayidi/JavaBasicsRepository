96894aed6d703d210587490b8c3c0c627d74962d
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 11:47:23 -0500

    Some Introductory points regarding the Abstract Concept in Java Programming Language were added to the Introduction.java file.

--- Basic_Java/src/com/javaabstraction/Introduction.java
+++ Basic_Java/src/com/javaabstraction/Introduction.java
@@ -1,9 +1,27 @@
 package com.javaabstraction;
 
 public class Introduction {
-
+	/* Java Abstraction - 
+	 	I. It is a process of hiding implementation details and showing only functionality to the User.
+	 	II. In java in Abstraction Point of view - two types of methods in Java,
+	 		i) Concrete Methods. (Methods with body)
+	 			Ex:
+	 				public void add()
+	 				{
+	 					statement I;
+	 					statement II;
+	 					-----------
+	 				}
+	 				Suppose in a Class there are n methods and all the n methods are Concrete Methods, then that Class can be defined as a Java Class.
+	 		ii) Abstract Methods. (Methods without body)
+	 			Ex:
+	 				public void sum();
+	 				If there is a class with n methods and if one or all the methods are Abstract Methods then we can say that Class is an Abstract Class.
+	 	III. If we know the method name but don't know the functionality of that method, then we can use Abstract Methods.
+	 	IV. Java Class contains 100% Concrete Methods.
+	 	V. Abstract Class contains one or more Abstract Methods.
+	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
