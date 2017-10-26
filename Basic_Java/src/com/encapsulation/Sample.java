e186191e9e1e1fcae28989101364fbbcd14efa48
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:52:25 -0500

    Accessing the setName() method from Child Class Sample.java.

--- Basic_Java/src/com/encapsulation/Sample.java
+++ Basic_Java/src/com/encapsulation/Sample.java
@@ -5,6 +5,8 @@ public class Sample extends SampleClass{
 	public static void main(String[] args) {
 		Sample o = new Sample();
 		System.out.println(o.getName());//Using getter Method we are accessing the private field(Variable courseName).
+		o.setName("Advanced Java");
+		System.out.println(o.getName());
 	}
 
 }
