6b33b1fb5522fd346a54528c932f49937f452b81
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:04:03 -0500

    Some important points regarding the Java Interfaces concept were added to the Introduction.java file in com.javainterfaces package.

--- Basic_Java/src/com/javainterfaces/Introduction.java
+++ Basic_Java/src/com/javainterfaces/Introduction.java
@@ -1,9 +1,25 @@
 package com.javainterfaces;
 
 public class Introduction {
-
+	/* Interfaces - Selenium:
+		Selenium IDE has User Interface (Integrated Development Environment) 
+	 	Selenium WebDriver is a Programming Interface.
+	 	UFT/QTP has both IDE as well as Programming Interface.
+	*/
+	
+	/* Java Interfaces - 
+	 	I. Interface is a Java type Definition block which have only Abstract Methods.
+	 	II. All Interface Methods are public and abstract by default.
+	 	III. static and final non access modifiers are not allowed for Interface Methods.
+	 	IV. In Interfaces Variables have to be initialized at the time of declaration.
+	 		Ex:
+	 			int a; //Incorrect regarding the Interfaces
+	 			int a = 25; //Correct regarding the Interfaces
+	 	V. In Interfaces Variables are public, static and final by default.
+	 	VI. For Interfaces implements keyword has to be used.
+	*/
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
