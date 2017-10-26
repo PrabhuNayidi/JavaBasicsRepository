0d275569a8ca8a8f62476966ea69ba58810abb99
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 19:27:31 -0500

    Sample code on protected access modifier was added to the file Class1.java.

--- Basic_Java/src/com/javainheritance/Class1.java
+++ Basic_Java/src/com/javainheritance/Class1.java
@@ -1,10 +1,16 @@
 package com.javainheritance;
 
 public class Class1 {
-
+	protected int a = 400,b = 1000;
+	protected void add()
+	{
+		System.out.println("The Sum of values of a and b is : " + (a+b));
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Class1 obj1 = new Class1();
+		System.out.println("The Value of a in Class1 is : " + obj1.a);
+		System.out.println("The Value of b in Class1 is : " + obj1.b);
+		obj1.add();
 	}
 
 }
