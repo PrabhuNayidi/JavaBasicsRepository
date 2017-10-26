5895bbf3bd58ba7a922c598fbf8e7a5561364b02
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:11:42 -0500

    Some Abstract Methods were added to the SampleInterface1.java file. A new Java Class file with name SampleClass1.java was added to same package o implement the Abstract Methods in SampleInterface1.java.

---
+++ Basic_Java/src/com/javainterfaces/SampleClass1.java
@@ -0,0 +1,9 @@
+package com.javainterfaces;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Basic_Java/src/com/javainterfaces/SampleInterface1.java
+++ Basic_Java/src/com/javainterfaces/SampleInterface1.java
@@ -1,5 +1,8 @@
 package com.javainterfaces;
 
 public interface SampleInterface1 {
-
+	public void myName();
+	public void gender();
+	public void age();
+	public void address();
 }
