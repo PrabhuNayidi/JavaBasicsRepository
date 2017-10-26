0af9f7abd22ac56e7939831027a8a50900e36a64
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 19:04:45 -0500

    A couple lines of code was changed in ClassB.java file.

--- Basic_Java/src/com/javainheritance/ClassB.java
+++ Basic_Java/src/com/javainheritance/ClassB.java
@@ -1,11 +1,10 @@
 package com.javainheritance;
 
 public class ClassB extends ClassA{
-
+	int a = 50;
+	int b = 40;
 	public static void main(String[] args) {
 		ClassB o2 = new ClassB();
-		o2.a = 50;
-		o2.b = 40;
 		System.out.println("The value of a from ClassA is : " + o2.a);
 		System.out.println("The value of b from ClassA is : " + o2.b);
 		o2.add();
