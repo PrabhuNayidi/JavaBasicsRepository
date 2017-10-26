00561c62d9ffc2b96b58ae2f8ff2f129e263370c
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-25 19:10:45 -0500

    Some Code was added to ClassC.java file to explain the Multi Level Inheritance concept inn Java Programming Language.

--- Basic_Java/src/com/javainheritance/ClassC.java
+++ Basic_Java/src/com/javainheritance/ClassC.java
@@ -1,10 +1,16 @@
 package com.javainheritance;
 
-public class ClassC {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class ClassC extends ClassB {
+	int a = 1;
+	int b = 2;
+	public void add()
+	{
+		System.out.println("The Sum of Values of a and b in ClassC is : " + (a+b));
 	}
-
+	public static void main(String[] args) {
+		ClassC o3 = new ClassC();
+		System.out.println("The Value of a in ClassC is : " + o3.a);
+		System.out.println("The Value of a in ClassC is : " + o3.b);
+		o3.add();
+	}
 }
