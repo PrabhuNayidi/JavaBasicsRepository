82c0c948b78b0b953784544c2a46579bf1db4ac7
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 13:17:25 -0500

    Implementation of the Abstract Methods in SampleClass1.java file.

--- Basic_Java/src/com/javainterfaces/SampleClass1.java
+++ Basic_Java/src/com/javainterfaces/SampleClass1.java
@@ -1,9 +1,32 @@
 package com.javainterfaces;
 
-public class SampleClass1 {
+public class SampleClass1 implements SampleInterface1{
 
-	public static void main(String[] args) {
-
+	@Override
+	public void myName() {
+		System.out.println("My Name is : FirstName + LastName");
 	}
 
+	@Override
+	public void gender() {
+		System.out.println("My Gender is : Male/Female/Other");
+	}
+
+	@Override
+	public void age() {
+		System.out.println("My Age is : SomeNumber");
+	}
+
+	@Override
+	public void address() {
+		System.out.println("My Address is : SomeSampleAddress");
+	}
+	
+	public static void main(String[] args) {
+		SampleClass1 obj = new SampleClass1();
+		obj.myName();
+		obj.gender();
+		obj.age();
+		obj.address();
+	}
 }
